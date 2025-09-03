d55a40f0109c0def1ede9b3cbd5dd61baff76416
thrila <ifeanyindubizu503@gmail.com.com>
2025-09-03 01:40:02 +0100

    feat: add svgr conf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true, // scale to 1em by default
+              titleProp: true, // allow <Logo title="..."/>
+            },
+          },
+        ],
+        as: "*.js", // treat output as JS module
+      },
+    },
+  },
 };
 
 export default nextConfig;
